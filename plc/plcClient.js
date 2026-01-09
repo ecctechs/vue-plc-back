75c8c256f516fe4e32d1c1745fd54d10f5894ce6
TheerasakRung <rungworking@gmail.com>
2026-01-09 10:07:28 +0700

    Reapply "performance"
    This reverts commit 035b00d6a6f5ab1c32b0e64866e35863d8401167.

--- plc/plcClient.js
+++ plc/plcClient.js
@@ -55,7 +55,9 @@ async function readPLC(device) {
     const res = await client.readCoils(coilAddress, length);
     const value = res.response.body.values[0] ? "ON" : "OFF";
 
-    await saveLog(device, value);
+
+    // await saveLog(device, value);
+    await saveIfChanged(device, value);
     return value;
   }
 
